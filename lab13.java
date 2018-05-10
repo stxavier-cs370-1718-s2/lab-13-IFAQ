966bf657151a88268b68579c422e198b51d33d95
IFAQ <IFAQ@users.noreply.github.com>
2018-05-10 13:22:08 -0400

    Initialization

---
+++ lab13.java
@@ -0,0 +1,7 @@
+// Lab13
+
+// Lab 13 is an open-ended lab with no specific answer key.
+
+// Students create their own programs which need to be graded 
+// using the criteria stated in the lab assignment.
+
